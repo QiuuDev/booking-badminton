8dc0bd198ae92604027594bdddceabcd8cd73065
QiuDevv <qodriodi91@gmail.com>
2026-01-11 09:50:12 +0800

    membuat untuk seedernya

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+{
+    \App\Models\User::create([
+        'name' => 'Admin Qodri',
+        'email' => 'admin@test.com',
+        'password' => bcrypt('password123'),
+        'role' => 'admin'
+    ]);
+
+    \App\Models\User::create([
+        'name' => 'Member Syaddad',
+        'email' => 'member@test.com',
+        'password' => bcrypt('password123'),
+        'role' => 'Member'
+    ]);
+}
+}
